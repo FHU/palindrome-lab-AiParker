bca33ea0fffafb493f6e80ef3c16849981690a89
AiParker <jordan.parker@students.fhu.edu>
2024-02-10 18:00:12 -0600

    Update Draft 1
    Added draft 1

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    pass
+    word = word.replace(" ", "").lower()
+
+    return word == word[::-1]
 
 if __name__ == '__main__': 
     #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    user_input = input("Enter a word: ")
+    print(palindrome(user_input))
